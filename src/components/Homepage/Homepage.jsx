355df16de5cae74a606884d0b2acea2082b0635d
kamal-kishor-rajput <90085564+kamal-kishor-rajput@users.noreply.github.com>
2022-07-07 01:41:29 +0530

    numeric input testing

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -4,6 +4,8 @@ export const Homepage = () => {
   return (
     <div>
       <h2>Read an article help us to plant a tree.</h2>
+      <input type="number" />
+      <input inputMode="numeric" />
     </div>
   );
 };
