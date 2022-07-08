48cc28b80b9dfb6a7a31c4076d1ce2330ddd2065
kamal-kishor-rajput <90085564+kamal-kishor-rajput@users.noreply.github.com>
2022-07-08 08:50:28 +0530

    mobile link

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -3,9 +3,50 @@ import "./Homepage.css";
 export const Homepage = () => {
   return (
     <div>
-      <h2>Read an article help us to plant a tree.</h2>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2960092495542092"
-     crossorigin="anonymous"></script>
+
+      <div className="homepage-comtainer">
+        <div><iframe
+        title="earphone"
+        sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin"
+        style={{width:"120px",height:"240px"}}
+        marginWidth="0" marginHeight="0"
+        scrolling="no" frameBorder="0"
+        src="//ws-in.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=IN&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=twoplantslife-21&language=en_IN&marketplace=amazon&region=IN&placement=B071Z8M4KX&asins=B071Z8M4KX&linkId=4ed02cbf118ca5594328bf2955f79e91&show_border=true&link_opens_in_new_window=true">
+        
+        </iframe></div>
+
+        <div>
+          <iframe
+            title="earphone"
+             sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin"
+        style={{width:"120px",height:"240px"}}
+        marginWidth="0" marginHeight="0"
+        scrolling="no" frameBorder="0"
+            src="//ws-in.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=IN&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=twoplantslife-21&language=en_IN&marketplace=amazon&region=IN&placement=B07S9S86BF&asins=B07S9S86BF&linkId=ef8f4e9dc8f9f64db0bbaae9283e52e3&show_border=true&link_opens_in_new_window=true"></iframe>
+        </div>
+      <div>
+        <iframe
+          sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin"
+          title="mobile"
+          
+          style={{width:"120px",height:"240px"}}
+          marginWidth="0" marginHeight="0"
+          scrolling="no" frameBorder="0"
+          src="//ws-in.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=IN&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=twoplantslife-21&language=en_IN&marketplace=amazon&region=IN&placement=B08444S68L&asins=B08444S68L&linkId=1fc09c859c50cd1e1903549e2dae5e48&show_border=true&link_opens_in_new_window=true"></iframe>
+        </div>
+        <div>
+          <iframe
+            
+            sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin"
+          title="mobile"
+          
+          style={{width:"120px",height:"240px"}}
+          marginWidth="0" marginHeight="0"
+          scrolling="no" frameBorder="0"
+            src="//ws-in.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=IN&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=twoplantslife-21&language=en_IN&marketplace=amazon&region=IN&placement=B097YGG9PQ&asins=B097YGG9PQ&linkId=005c75c47dd5e05a55b9767b2d4d0cdc&show_border=true&link_opens_in_new_window=true"></iframe>
+        </div>
+          </div>
+      
     </div>
   );
 };
